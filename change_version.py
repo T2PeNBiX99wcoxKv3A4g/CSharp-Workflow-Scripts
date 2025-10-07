5259e12be9ad38b1c652be982efa552e87d6f380
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-08 06:41:41 +0800

    feat: Enhance version update logic and allow optional README updates

--- change_version.py
+++ change_version.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import os
-import typer
 
+import typer
 from icecream import ic
 
 
@@ -9,19 +9,52 @@ class OldVersionNotFound(Exception):
     def __init__(self, *args, **kwargs): pass
 
 
-def main(file_path: str, find_keyword: str, new_version: str, debug: bool = False):
+remove_chars_in_new_version = [
+    "\"",
+    "v"
+]
+
+remove_chars_in_version_path = [
+    "\n",
+    ";",
+    "\"",
+    ","
+]
+
+
+def string_handle(input_string: str, char_list: list[str]) -> str:
+    ret_string = input_string
+    for char in char_list:
+        ret_string = ret_string.replace(char, "")
+    return ret_string.strip()
+
+
+def extra_find_keywords_check(extra_find_keyword_list: list[str], line: str) -> bool:
+    for extra_find_keyword in extra_find_keyword_list:
+        if line.find(extra_find_keyword) < 0:
+            return False
+    return True
+
+
+def main(file_path: str, find_keyword: str, new_version: str, debug: bool = False, split_keyword: str = "=",
+         change_readme: bool = False, readme_file_path: str = "./README.md", extra_find_keywords: str = ""):
     if debug:
         ic.enable()
     else:
         ic.disable()
 
-    ic(file_path, find_keyword, new_version)
+    ic(file_path, find_keyword, new_version, split_keyword, change_readme, readme_file_path, extra_find_keywords)
     if not os.path.isfile(file_path):
         raise FileNotFoundError(file_path)
 
     ic(new_version)
 
-    new_version = new_version.replace('"', '').replace("v", "")
+    new_version = string_handle(new_version, remove_chars_in_new_version)
+    extra_find_keyword_list = extra_find_keywords.split(",")
+
+    for i in range(len(extra_find_keyword_list)):
+        extra_find_keyword_list[i] = extra_find_keyword_list[i].strip()
+    ic(extra_find_keyword_list)
 
     if not new_version[0].isdigit():
         new_version = new_version[1:]
@@ -36,25 +69,29 @@ def main(file_path: str, find_keyword: str, new_version: str, debug: bool = Fals
         for line in file.readlines():
             start = line.find(find_keyword)
 
-            ic(line)
+            ic(line, start)
 
             if start < 0:
                 old_text += line
                 continue
 
-            version_paths = line.split("=")
-
-            if len(version_paths) < 1:
+            if start != 0 and line[start - 1] != ' ':
                 old_text += line
                 continue
 
-            if line.find("string") < 0:
+            if len(extra_find_keyword_list) > 0 and not extra_find_keywords_check(extra_find_keyword_list, line):
                 old_text += line
                 continue
 
-            ic()
+            version_paths = line.split(split_keyword)
 
-            old_version = version_paths[1].replace("\n", "").replace(";", "").replace("\"", "").strip()
+            if len(version_paths) < 2:
+                old_text += line
+                continue
+
+            ic(version_paths)
+
+            old_version = string_handle(version_paths[1], remove_chars_in_version_path)
             typer.echo(f'Old version: {old_version}, New version: {new_version}')
             old_text += line
 
@@ -63,6 +100,21 @@ def main(file_path: str, find_keyword: str, new_version: str, debug: bool = Fals
             raise OldVersionNotFound(f'Old version is not found')
 
         new_text = old_text.replace(old_version, new_version)
+        file.seek(0)
+        file.truncate(0)
+        file.write(new_text)
+        file.close()
+
+    if not change_readme:
+        return
+
+    if not os.path.isfile(readme_file_path):
+        raise FileNotFoundError(readme_file_path)
+
+    with open(readme_file_path, "r+") as file:
+        old_text = "".join(file.readlines())
+        new_text = old_text.replace(old_version, new_version)
+        ic(old_text, new_text)
         file.seek(0)
         file.truncate(0)
         file.write(new_text)
